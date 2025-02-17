5b3f15376be9d299e5fce7c90f08851839dd3bd1
C4lice <laura.aupetit002@gmail.com>
2025-02-17 09:18:42 +0100

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-int main(void){printf("with proper grammar, but the outcome is a piece of art,\n");return 0;}
+#include <stdio.h>
+/**
+ *main - ...
+ *
+ * Return: ...
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
