82d8a2657d66376039bea479556d94c09581c9d9
C4lice <laura.aupetit002@gmail.com>
2025-02-19 09:37:08 +0100

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - ...
+ *
+ * Return: ...
+ */
+int main(void)
+{
+	int i;
+
+	for (int i = 48 ; i <= 57 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
