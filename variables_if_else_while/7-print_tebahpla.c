552051e87690cd11569a3cc8454fc6285dc9dd29
C4lice <laura.aupetit002@gmail.com>
2025-02-19 09:54:43 +0100

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - ...
+ *
+ * Return: ...
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
