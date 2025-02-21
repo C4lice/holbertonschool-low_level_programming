b87397ba9fff88c6a805edb4d330e5539f2c5cd5
C4lice <laura.aupetit002@gmail.com>
2025-02-21 14:58:39 +0100

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,14 +4,13 @@
  *
  * Return: ...
  */
-
 void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		_putchar(letter);
+	_putchar(letter);
 	}
 	_putchar('\n');
 }
