57e0a4082022f36d4fe8f788a5caa4b48319213a
C4lice <laura.aupetit002@gmail.com>
2025-02-23 15:35:35 +0100

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * int _islower(int c) - ...
+ * _islower - ...
+ * @c: ...
  *
  * Return: ...
  */
-int _islower (int c)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
