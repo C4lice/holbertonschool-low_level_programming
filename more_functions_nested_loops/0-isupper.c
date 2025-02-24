de0f6b8833d8b3b69e46bf2bb5b4124790fa628d
C4lice <laura.aupetit002@gmail.com>
2025-02-24 14:30:09 +0100

    0 et readme

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - ...
+ * @c: ...
+ * Return: ...
+ */
+int _isupper(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}
---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+Projets c- more functions, more nested loops
+
+10 tasks
