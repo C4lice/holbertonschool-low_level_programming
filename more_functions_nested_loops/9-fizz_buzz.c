6325440c2df53696cd9b4f46363a4b5f19cc7ed6
C4lice <laura.aupetit002@gmail.com>
2025-02-26 10:48:24 +0100

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 /**
  * main - ...
- * @num: ...
  * Return: ...
  */
 int main(void)
 {
-	int num = 1;
+	int num = 0;
+
 	while (num++ < 100)
 
 		if ((num % 3 == 0) && (num % 5 == 0))
-				printf("FizzBuzz ");
-
-				else if ((num % 3) == 0)
-				printf("Fizz ");
-
+			printf("FizzBuzz ");
+		else if ((num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
 		else if ((num % 5) == 0)
 		{
 			if (num != 100)
