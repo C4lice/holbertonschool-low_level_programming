30551123642a795c5f487157f329f995d120a94f
C4lice <laura.aupetit002@gmail.com>
2025-03-07 20:32:06 +0100

    1-args.c 2-args.c 3-mul.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - ...
+ * @argc: type de compteur d'arguments int
+ * @argv: strings qui contiennes des arguments
+ * Return: ...
+ */
+int main(int argc, __attribute__((unused))char **argv)
+{
+    printf("%d\n", argc - 1);
+    return (0);
+}
---
+++ argc_argv/3-mul.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - print the multiplacation of 2 numbers
+ * @argc: type de compteur d'arguments int
+ * @argv: strings qui contiens des arguments
+ *Return: 0 if success
+ */
+int main(__attribute__((unused))int argc, char **argv)
+{
+	int i = 1;
+	int result = 1;
+	int num = atoi(argv[i]);
+
+	while (argv[i])
+	{
+		num = atoi(argv[i]);
+
+		if (argc < 3 || argc > 5)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			result *= num;
+		}
+
+		i++;
+	}
+	printf("%d\n", result);
+	return (0);
+}
