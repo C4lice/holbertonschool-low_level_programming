d65b7b02881cb07cbefb99a7390e8509a7cae9d1
C4lice <laura.aupetit002@gmail.com>
2025-03-11 14:55:54 +0100

    main.h

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -64,5 +64,9 @@ int _sqrt_root(int n,int x);
 int is_prime_number(int n);
 int is_prime(int n, int x);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
 #endif
