e683d7cb63688abf4053a0bb8fcff8e5b70ca42d
C4lice <laura.aupetit002@gmail.com>
2025-04-04 17:51:21 +0200

    install first file

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * read_textfile - open & read a textfile to save char in a buffer and print
+ * in the stdout the text file
+ *
+ * @filename: pointer to the mem adress of the file
+ * @letters: the number of letters/byte we will allocate the buffer with
+ *
+ * Return: 0 if any operation fail, the number of printed letters otherwise
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t n_read, n_written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+	return (0);
+	}
+
+	n_written = write(STDOUT_FILENO, buffer, n_read);
+	if (n_written == -1 || n_written != n_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (n_written);
+}
---
+++ file_io/README.md
@@ -0,0 +1,1 @@
+tasks 0 a 3 du projet file_io
